d727622f9ca7e258709490f9594f9e2eac5f6b51
Antoine BEDIN <antoinebedin@orange.fr>
2025-05-11 11:31:26 +0200

    unified work on CGI and work on POST/DELETE requests

--- tests2.0/cpp_files/main.cpp
+++ tests2.0/cpp_files/main.cpp
@@ -6,7 +6,7 @@
 /*   By: administyrateur <administyrateur@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 15:09:46 by ilevy             #+#    #+#             */
-/*   Updated: 2025/05/03 17:36:03 by administyra      ###   ########.fr       */
+/*   Updated: 2025/05/11 11:27:08 by administyra      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 int 			LOGSV = 1;
 volatile int	g_global_signal;
 
+// Modifies the global communication variable to indicate that a SIGINT was received
+// to allow the <Server> instance to exit gracefully
 void	handleSigint(int sigNum)
 {
 	if (sigNum == SIGINT)
@@ -28,8 +30,9 @@ int	main(int ac, char **av)
 	g_global_signal = 0;
 	std::signal(SIGINT, &handleSigint);
 
-	
-	Server		serv("webserv.conf");
+	Server		serv;
+	if (serv.initServer("webserv.conf")) // #f : make this an argument of the program
+		return (1);
 	if (serv.startServer())
 		return (1);
 	if (serv.serverLoop())
@@ -98,27 +101,16 @@ int	main(int ac, char **av)
 // CODE STRUCTURE //
 ////////////////////
 
-+ build true server loop with <poll>
-+ test some <write> in a socket (see results with curl/nc)
-+ catch signals to shutdown server cleanly (close all sockets and return)
-- parsing of config file (always define a default when a directive is absent)
-	\ set port and name of each (virtual) server : `listen address:port`, `server_name name`
-		/!\ server name can be an IP
-	\ set default error pages : `error_page errcode page_uri`
-	\ set maximum allowed size for client request bodies : `client_max_body_size size`
-		/!\ size expressed in some defined unit, or accept characters 'K/M/G' for units ?
-	\ set and configure routes
-		~ define route prepended to a certain requested path : `location path {root prepend}` -> true path is prepend/path
-		~ define accepted HTTP methods for a route : `limit_except method` inside location
-		~ define HTTP redirect (code 301) : `location oldpath {return 301 newpath}`
-	\ enable|disable directory listing : `autoindex on|off` inside location
-	\ define a default file to serve when request ends in '/' : `index filename` in location
-	\ define file extensions for which CGI should be executed, for POST and GET : custom
-	\ allow uploading files with POST/PUT methods, define storage location : custom
-- parsing des requetes GET/POST/DELETE
-- handling of CGIs
-- correct handling of errors (cleaning/closing sockets when necessary)
-	notably POLLERR or a read failure on a dedicated socket : should close this socket only, not whole server
+- role of each class :
+	\ Server : reads the config file to create (and manage, using function <poll> and instances of Client)
+		the main sockets listening for new connections and the dedicated sockets communicating with a client
+	\ Client : reacts to the POLLIN (POLLOUT) events received on a dedicated socket
+		by reading (writing) into the socket and creating the instances of Request/Response for each request
+	\ Request : parses a HTTP request, and does nothing else (therefore, it has no refs to vservers list)
+	\ Response : generates the response to a given instance of Request, by
+		~ identifying at which location of which virtual server the Request is directed
+		~ searching this device's arborescence for files / executing CGI scripts
+		~ creating the HTTP response (status code, headers, body)
 
 - handle multiple "virtual" servers :
 	\ by parsing cfg file, we have a set of ports+address on which to read
@@ -148,27 +140,87 @@ int	main(int ac, char **av)
 		/!\ the server polls both main and dedicated sockets, but events on dedicates sockets are handled
 			by the corresponding instance of <Client>
 
-///////////////
-// TODO MISC //
-///////////////
+//////////
+// MISC //
+//////////
 
+- differences apres la mise en commun :
+	\ split des methodes de Response entre deux fichiers, un pour les requetes CGI et un pour les autres
+		(il y aura sans doute 3 fichiers a terme vu le travail que va demander l'upload)
+	\ deplacement des methodes <extractFromURL> et <extractFromHost> dans la classe Request,
+		pour qu'elle soit chargee de tout le travail de parsing (et de celui-la seulement)
+	\ passage des attributs de Request en public pour etre plus facilement accessibles :
+		ca me semble justifiable vu que c'est une classe de parsing et non de logique de serveur
+	\ Client recoit les requetes dans une std::string <_requestHeaders> jusqu'a la fin des headers,
+		puis cree une instance de Request qui allouera un char* <_body> pour stocker la suite de la requete (le body)
+	\ Client envoie ses reponses a partir d'un char* qui a ete alloue par une instance de Response qu'il a creee
+	\ dans la gestion de GET/POST/DELETE sans CGI, la recherche du chemin vers le fichier et les autres checks
+		ont ete deplaces dans plusieurs methodes pour que ce soit plus lisible
+- a adapter dans le fichier de conf pour tester en local :
+	\ modifier l'IP sur laquelle ecoute le second serveur (comqnde `ip a` pour IP privee de l'appareil)
+	\ modifier les path definis dans les directives 'alias'/'root'
+- lignes de tests pour les differentes requetes, avec `nc` :
+	\ GET (non-CGI) : ```printf "GET /images/truc.html HTTP/1.1\r\nHost:localhost:8080\r\n\r\n" | nc 127.0.0.1 8080```
+	\ POST (non-CGI) : ```printf "POST /images/myfile.txt HTTP/1.1\r\nHost:localhost:8080\r\nContent-Length:3\r\nContent-Type:text/plain\r\n\r\nabc" | nc 127.0.0.1 8080```
+	\ DELETE : ```printf "DELETE /images/myfile.txt HTTP/1.1\r\nHost:localhost:8080\r\n\r\n" | nc 127.0.0.1 8080```
+	\ POST (CGI) : ```printf "POST /app/handleform.py HTTP/1.1\r\nHost:localhost:8080\r\nContent-Length:17\r\nContent-Type: application/x-www-form-urlencoded\r\n\r\nname=aa&surname=b" | nc 127.0.0.1 8080```
+
+//////////
+// TODO //
+//////////
+
++ build true server loop with <poll>
++ test some <write> in a socket (see results with curl/nc)
++ catch signals to shutdown server cleanly (close all sockets and return)
++ parsing of config file (always define a default when a directive is absent)
+	\ set port and name of each (virtual) server : `listen address:port`, `server_name name`
+		/!\ server name can be an IP
+	\ set default error pages : `error_page errcode page_uri`
+	\ set maximum allowed size for client request bodies : `client_max_body_size size`
+		/!\ size expressed in some defined unit, or accept characters 'K/M/G' for units ?
+	\ set and configure routes
+		~ define route prepended to a certain requested path : `location path {root prepend}` -> true path is prepend/path
+		~ define accepted HTTP methods for a route : `limit_except method` inside location
+		~ define HTTP redirect (code 301) : `location oldpath {return 301 newpath}`
+	\ enable|disable directory listing : `autoindex on|off` inside location
+	\ define a default file to serve when request ends in '/' : `index filename` in location
+	\ define file extensions for which CGI should be executed, for POST and GET : custom
+	\ allow uploading files with POST/PUT methods, define storage location : custom
+- parsing of requests GET/POST/DELETE
+- handling of CGIs
+- correct handling of errors (cleaning/closing sockets when necessary)
+	\ POLLERR or a read failure on a dedicated socket : should close this socket only, not whole server
+	\ all cases where a HTTP request (or response generated by CGI) is wrongly formatted
+	\ all cases where config file has errors (eg. 0 virtual servers defined)
+- handle chunked requests and responses, limit max request size, handle non-text files as entry
+
++ remplacer les pair<int,short> par un type portaddr, avec un typedef
 - tests
 	\ faire quelques tests avec nginx pour voir le contenu des headers
 	\ tests avec Firefox/curl/nc + eventuellement un script de test
 	\ essayer d'utiliser wireshark
-- remplacer les pair<int,short> par un type portaddr, avec un typedef
-
-- petites differences apres la mise en commun :
-	\ ajout de la gestion de SIGINT
-	\ retrait de la majuscule pour les headers ne definissant pas une classe
-	\ ajout du parsing du fichier de cfg (qui n'est pas tres elegant)
-	\ deplacement du log des elements de Request dans une methode de la classe Request
-	\ les instances de Client recoivent la liste des serveurs virtuels en parametre de constructeur,
-		pour leur permettre d'identifier a quel serveur virtuel la requete en cours est adressee
-	\ le Client envoie son placeholder de reponse (pas reconnu par Firefox puisque pas HTTP mais visible avec netcat par ex),
-		potentiellement en plusieurs paquets en utilisant _nBytesSent_tmp comme marqueur
-	\ allegement du fichier de conf pour que les tests soient plus lisibles en ecoutant sur un seul portaddr,
-		mais il faudra refaire des tests avec pleins de serveurs virtuels sur plein de portaddrs
+- verifier que le vserver selectionne par defaut est bien le premier vserver de la liste
+	(depend du choix de container vector/set dans le t_mainSocket)
+- en local a 42, utiliser le nom du poste a la place de localhost pour simuler un nom de serveur
+- lire le salon webserv pour des tests de charge
+- verifier les autorisations sur les fonctions de C et C++ (malloc, free, strdup, bzero...)
+	-> sur les erreurs de malloc : faire un petit try catch global pour dire quùon ne crash pas sur erreur de malloc
+		(verifier que la structure par classe avec des destructeurs qui nettoient
+		 permet de tout bien free meme si on ne le fait pas manuellement)
+- rendre clair dans le code ce qui est une erreur fatale ou non
+- faire en sorte que les malformed request close la connexion
+	(probablement un bool dans Client qui demande de close la connexion quand l'envoi est fini plutot que de passer en read,
+		et donc un check du retour de response dans Server pour detruire l'instance une fois qu'elle a close la connexion)
+- commentaire sur le fait qu'une malformed request avec un body mais pas de "content-length" va desynchroniser la connexion
+- verifier tous les champs du fichier de config et leur implementation (ex. index.html, max body size)
+- implementer la reception des chunked requests, verifier si on doit/peut implementer des chunked response
+- mecanisme de protection contre plusieurs clients qui chargent des gros fichiers en meme temps
+	(exemple : un compteur global du nombre d'octets de memoire heap utilisee)
+- actuellement la reponse est ecrite en une fois dans le socket si possible,
+	verifier si on devrait definir une taille de buffer de sortie
+- verifier le fonctionnement des autres methodes d'envoi de donnees de formulaire (querystring, formulaire)
+- verifier si il y a un truc a gerer avec les headers composes de plusieurs champs separes par des ';'
+- gerer le souci de determination du path : si on ne trouve pas de '.' pour l'extension, le dernier element est le filename
 
 - verifications sur la gestion des connexions avec le client :
 	\ conditions de fin de requête = "\r\n\r\n" et pas de content length,
